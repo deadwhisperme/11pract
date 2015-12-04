74a307143e89de386fb70762241ae3df63a2a27a
deadwhisperme <ekaterinacydenzhapova@mail.ru>
2015-12-04 23:41:28 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,31 +1,14 @@
-class Point:
-        def __init__(self, x = 0, y = 0):
-               
 
-                if type(x) ==str:
-                        a=x.split(',')
-                        if len(a)==2:
-                                self.x = int(a[0])
-                                self.y = int(a[1])
-                else:
-                      self.x = x
-                      self.y = y
-
-        def print(self):
-                print(self.x, self.y)
-        def __str__(self):
-                return '(' + str(self.x) +','+ str(self.y) + ')'
-        def __lt__(self, other):
-                return self.x < other.x or self.x == other.x and self.y < other.y
-        def __add__(self, other):
-                return Point(self.x + other.x, self.y + other.y)
-
-        def __sub__(self, other):
-                return Point(self.x-other.x, self.y-other.y)
-        def __gt__(self,other):
-                if (self.x**2+self.y**2)>(other.x**2+other.y**2):
-                    return True
-                else:
-                    return False
-        def __truediv__(self,N):
-                return Point(self.x/N, self.y/N)
+class point:
+    def __init__(self,strk='0,0'):
+        self.x=float(strk[:strk.index(',')])
+        self.y=float(strk[strk.index(',')+1:])
+    def __str__(self):
+        return '('+str(self.x)+','+str(self.y)+')'
+    def __add__(self,other):
+        return point(str(self.x+other.x)+','+str(self.y+other.y))
+    def __sub__(self,other):
+        return point(str(self.x-other.x)+','+str(self.y-other.y))
+A=point(input())
+B=point(input())
+print(A+B,A-B)
